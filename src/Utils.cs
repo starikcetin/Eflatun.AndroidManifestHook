6a6edacf3ce5c45111c31f2ca7c2964c55d5058b
starikcetin <cetinsamedtarik@gmail.com>
2019-10-28 02:48:30 +0300

    'upm release'

---
+++ src/Utils.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+using System.Text;
+
+namespace Eflatun.AndroidManifestHook
+{
+    internal static class Utils
+    {
+        public static string GetManifestPath(string basePath) {
+            var pathBuilder = new StringBuilder(basePath);
+            pathBuilder.Append(Path.DirectorySeparatorChar).Append("src");
+            pathBuilder.Append(Path.DirectorySeparatorChar).Append("main");
+            pathBuilder.Append(Path.DirectorySeparatorChar).Append("AndroidManifest.xml");
+            return pathBuilder.ToString();
+        }
+
+        public static string ToManifestString(this bool val)
+        {
+            return val ? "true" : "false";
+        }
+    }
+}
